9fea81844827f5c81add88b4b8b12965d8caf2a6
Jakob Aungiers <jakob@jakob-aungiers.com>
2015-11-27 15:36:41 +0000

    debugging more

--- ImgResizeApi.py
+++ ImgResizeApi.py
@@ -1,19 +1,19 @@
 import os
 from flask import Flask, render_template, request, send_file
-'''
+
 import time, datetime
 import random
 import numpy as np
 import cv2
 import urllib2
-'''
+
 ImgResizeApi = Flask(__name__)
 
 @ImgResizeApi.route('/')
 def main():
-	#cleanup_temps()
+	cleanup_temps()
 	return render_template('index.html')
-'''
+
 @ImgResizeApi.route('/api')
 def imgRs():
 	basePath = 'static/images/temp/'
@@ -22,10 +22,12 @@ def imgRs():
 	height = request.args.get('h')
 	inpImg = fetchImg(imgUrl)
 	outImg = cv2.resize(inpImg, (int(width), int(height)))
-
+	'''
 	filename = str(time.time()) +  str(random.randrange(0,100000)) + '.png'
 	cv2.imwrite(basePath + filename, outImg)
 	return send_file(basePath + filename, mimetype='image/png')
+	'''
+	return render_template('index.html')
 
 def fetchImg(imgUrl):
 	req = urllib2.urlopen(imgUrl)
@@ -39,6 +41,6 @@ def cleanup_temps():
 	for f in os.listdir(basePath):
 		if os.path.getctime(basePath + f) < (time.time() - deleteDelay):
 			os.remove(basePath + f)
-'''
+
 if __name__ == '__main__':
 	ImgResizeApi.run()
